0e5c68a4274b8548d04fe387183451f4ad8ad896
Luis Carlos - [ Frontend ] <luiscarlos-dev@outlook.com>
2023-03-05 13:48:55 -0300

    ajust for deploy

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from 'msw'
-import { charactersHandlers } from './domians/characters/characters.handlers'
+import { charactersHandlers } from './domains/characters/characters.handlers'
 
 export const worker = setupWorker(...charactersHandlers)
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rick-and-morty-app',
   test: {
     globals: true,
     environment: 'jsdom',
